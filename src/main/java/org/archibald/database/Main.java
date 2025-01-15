bcb594b2ae5efb90270d1a74bc26e826221c8321
Rouxie25 <95306992+Rouxie25@users.noreply.github.com>
2025-01-15 06:47:27 +0100

    Added TODO tasks

--- src/main/java/org/archibald/database/Main.java
+++ src/main/java/org/archibald/database/Main.java
@@ -3,6 +3,14 @@ package org.archibald.database;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
+/* TODO
+* Commands player CRUD in database
+* Config file with database name
+* Log player sanctions in database (ban, kick)
+* Add player to database on join
+* Log player enter and leave
+* */
+
 public final class Main extends JavaPlugin {
 
     @Override
